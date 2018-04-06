978a1552977d162ad28e3fdde108e0d66e7f1edc
Megha Santhosh <meghasanthosh438@gmail.com>
2018-04-06 23:53:52 +0530

    events

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+  // events js starts here
+ 
+ $(document).ready(function(){
+    $('.modal').modal();
+  });
+
+  // events js ends here
