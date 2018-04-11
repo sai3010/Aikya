833493d8988a8bca64fdac3ce5786004afb8ee1c
sai3010 <saiprasanth2007@gmail.com>
2018-04-11 22:41:38 +0530

    added typewrting effect

--- js/index.js
+++ js/index.js
@@ -1,18 +1,34 @@
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+// $(document).ready(function () {
+//   $('.sidenav').sidenav()
+// })
 
-  // events js starts here
- 
- $(document).ready(function(){
-    $('.modal').modal();
-  });
+// // events js starts here
 
-  // events js ends here
-  
-  //homr page carousel begin
-$(document).ready(function(){
-  $('.slider').slider();
-});
+// $(document).ready(function () {
+//   $('.modal').modal()
+// })
 
-  //home page carousel end
+// // events js ends here
+
+// // homr page carousel begin
+// $(document).ready(function () {
+//   $('.slider').slider()
+// })
+
+// home page carousel end
+
+// odometer
+window.onload = function () {
+  setTimeout(function () {
+    odometer.innerHTML = 18
+  }, 1000)
+}
+
+// typeit js code
+new TypeIt('#title', {
+  strings: 'DEPARTMENT OF CSE PRESENTS',
+  speed: 100,
+  autoStart: true
+})
+
+// end of tyepit js
